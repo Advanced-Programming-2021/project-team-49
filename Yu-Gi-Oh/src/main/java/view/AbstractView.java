28ae2fb0fa7349682129b2d3be1639ee85f030d4
SalehShamloo <SLHHero@gmail.com>
2021-04-24 14:18:11 +0430

    write parser function

--- Yu-Gi-Oh/src/main/java/view/AbstractView.java
+++ Yu-Gi-Oh/src/main/java/view/AbstractView.java
@@ -1,8 +1,13 @@
 package view;
 
 public abstract class AbstractView {
-    protected static String getStringValueFromCommand(String string) {
-        String stringValue = new String();
-        return stringValue;
+    protected static String getStringValueFromCommand(String string, String[] command) {
+        String commandString = "--" + string;
+        String abbreviatedCommandString = "-" + string.charAt(0);
+        for (int i = 0; i < command.length - 1; i++)
+            if (commandString.equals(command[i])
+                    || commandString.equals(abbreviatedCommandString))
+                return command[i + 1];
+        return null;
     }
 }
