bfc28a78703850ac510c1b6709aa63e7505e146d
Amin <seyedaminahmadzadeh@gmail.com>
2021-05-01 16:34:25 +0430

    move game.Game properties to duelController and remove that

--- Yu-Gi-Oh/src/main/java/controller/DuelController.java
+++ Yu-Gi-Oh/src/main/java/controller/DuelController.java
@@ -1,12 +1,23 @@
 package controller;
 
+import model.game.Field;
+import model.game.Phase;
 import model.user.User;
 
 public class DuelController extends AbstractController {
+
     public static final String TITLE = "Duel Menu";
+
+    private final User guest;
+    private Field field;
+    private int player1WinCount;
+    private int player2WinCount;
+    private int drawCount;
+    private Phase currentPhase;
 
     public DuelController(MasterController masterController, User host, User guest) {
         super(masterController, host);
+        this.guest = guest;
     }
 
     public void run() {
