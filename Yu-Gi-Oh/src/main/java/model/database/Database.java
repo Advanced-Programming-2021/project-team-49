00c650e72786b5e445cd9d6c5c2d395a6c292647
Amin <seyedaminahmadzadeh@gmail.com>
2021-05-05 00:32:24 +0430

    add csvReader method

--- Yu-Gi-Oh/src/main/java/model/database/Database.java
+++ Yu-Gi-Oh/src/main/java/model/database/Database.java
@@ -1,11 +1,34 @@
 package model.database;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import model.user.User;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 // TODO: implement loading from file
 public class Database {
     private final Userbase userbase = new Userbase();
     private final Shop shop = new Shop();
+
+    public Database() {
+
+    }
+
+    public static CSVReader readFile(String address) throws IOException, CsvValidationException {
+        FileReader filereader = new FileReader(address);
+        CSVReader csvReader = new CSVReader(filereader);
+        String[] nextRecord;
+
+        while ((nextRecord = csvReader.readNext()) != null) {
+            for (String cell : nextRecord) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+        return csvReader;
+    }
 
     public Userbase getUserbase() {
         return userbase;
