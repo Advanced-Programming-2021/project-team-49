b67c41a011a6409c40fd24adbb83f7521293bf35
SalehShamloo <SLHHero@gmail.com>
2021-05-16 01:03:47 +0430

    fixed single flag handling

--- Yu-Gi-Oh/src/main/java/view/DeckBuilderView.java
+++ Yu-Gi-Oh/src/main/java/view/DeckBuilderView.java
@@ -36,27 +36,23 @@ public class DeckBuilderView extends AbstractView {
                 controller.activateDeck(deckName);
                 System.out.println("deck activated successfully!");
             } else if (input.startsWith("deck add-card")) {
-                String[] argumentNames = {"card", "deck"};
-                String[] flags = {"side"};
-                String[] arguments = getArguments(argumentNames, flags, input, "deck add-card");
+                String[] arguments = getArguments(new String[] {"card", "deck"}, new String[] {"side"},
+                        input, "deck add-card");
 
                 controller.addCardToDeck(arguments[0], arguments[1], isFlagUsedInCommand("side", input));
                 System.out.println("card added to deck successfully!");
             } else if (input.startsWith("deck rm-card")) {
-                String[] argumentNames = {"card", "deck"};
-                String[] flags = {"side"};
-                String[] arguments = getArguments(argumentNames, flags, input, "deck rm-card");
+                String[] arguments = getArguments(new String[] {"card", "deck"}, new String[] {"side"},
+                        input, "deck rm-card");
 
                 controller.removeCardFromDeck(arguments[0], arguments[1], isFlagUsedInCommand("side", input));
                 System.out.println("card removed from deck successfully!");
-            } else if (input.equals("deck show --all") || input.startsWith("deck show -a")) {
+            } else if (input.equals("deck show --all") || input.equals("deck show -a")) {
                 System.out.println(showAllDecks(controller.getUserActiveDeck(), controller.getUserDecks()));
-            } else if (input.equals("deck show --cards") || input.startsWith("deck show -c")) {
+            } else if (input.equals("deck show --cards") || input.equals("deck show -c")) {
                 System.out.println(showAllCards(controller.getOwnedCards()));
             } else if (input.startsWith("deck show")) {
-                String[] argumentNames = {"deck"};
-                String[] flags = {"side"};
-                String[] arguments = getArguments(argumentNames, flags, input, "deck show");
+                String[] arguments = getArguments(new String[] {"deck"}, new String[] {"side"}, input, "deck show");
 
                 System.out.print(showDeck(arguments[0], isFlagUsedInCommand( "side", input),
                         controller.getMonsters(arguments[0], isFlagUsedInCommand( "side", input)),
