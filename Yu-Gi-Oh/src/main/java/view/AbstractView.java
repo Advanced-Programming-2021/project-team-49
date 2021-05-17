d80410b5a3c12656a08318b4762d3a16d454cad4
SalehShamloo <SLHHero@gmail.com>
2021-05-17 11:55:33 +0430

    reworked input handling functions

--- Yu-Gi-Oh/src/main/java/view/AbstractView.java
+++ Yu-Gi-Oh/src/main/java/view/AbstractView.java
@@ -18,90 +18,52 @@ public abstract class AbstractView {
     }
 
     public static String[] getArguments(String[] argumentNames, String[] flags, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
+        String[] command = getCommand(input, prefix);
         String[] arguments = extractArgumentsFromCommand(argumentNames, command);
+        boolean[] flagConditions = findFlags(flags, command);
 
-        if (flags.length > 0)
-            removeFlagsFromCommand(flags, command);
-        if (command.size() > 0)
+        int commandCount = argumentNames.length * 2;
+        for (boolean flagCondition : flagConditions)
+            if (flagCondition)
+                commandCount++;
+
+        if (commandCount != command.length)
             throw new GameErrorException(INVALID_COMMAND_MESSAGE);
+
         return arguments;
     }
 
     public static String[] getArguments(String[] argumentNames, String flag, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
-        String[] arguments = extractArgumentsFromCommand(argumentNames, command);
-
-        if (flag != null)
-            removeFlagFromCommand(flag, command);
-        if (command.size() > 0)
-            throw new GameErrorException(INVALID_COMMAND_MESSAGE);
-
-        return arguments;
+        return getArguments(argumentNames, new String[] {flag}, input, prefix);
     }
 
     public static String[] getArguments(String[] argumentNames, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
+        String[] command = getCommand(input, prefix);
         String[] arguments = extractArgumentsFromCommand(argumentNames, command);
 
-        if (command.size() > 0)
+        if (command.length != 2 * arguments.length)
             throw new GameErrorException(INVALID_COMMAND_MESSAGE);
+
         return arguments;
     }
 
     public static String getArgument(String argumentName, String[] flags, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
-        String argument = extractArgumentFromCommand(argumentName, command);
-
-        if (flags.length > 0)
-            removeFlagsFromCommand(flags, command);
-        if (command.size() > 0)
-            throw new GameErrorException(INVALID_COMMAND_MESSAGE);
-        return argument;
+        return getArguments(new String[] {argumentName}, flags, input, prefix)[0];
     }
 
     public static String getArgument(String argumentName, String flag, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
-        String argument = extractArgumentFromCommand(argumentName, command);
-
-        if (flag != null)
-            removeFlagFromCommand(flag, command);
-        if (command.size() > 0)
-            throw new GameErrorException(INVALID_COMMAND_MESSAGE);
-        return argument;
+        return getArgument(argumentName, new String[] {flag}, input, prefix);
     }
 
     public static String getArgument(String argumentName, String input, String prefix) {
-        List<String> command = getCommandList(input, prefix);
-        String argument = extractArgumentFromCommand(argumentName, command);
-
-        if (command.size() > 0)
-            throw new GameErrorException(INVALID_COMMAND_MESSAGE);
-        return argument;
+        return getArguments(new String[] {argumentName}, input, prefix)[0];
     }
 
-    private static List<String> getCommandList(String input, String prefix) {
-        return new ArrayList<>(Arrays.asList(input.substring(prefix.length() + 1).split(" ")));
+    private static String[] getCommand(String input, String prefix) {
+        return input.substring(prefix.length() + 1).split(" ");
     }
 
-    private static void removeFlagsFromCommand(String[] flags, List<String> command) {
-        for (String flag : flags)
-            removeFlagFromCommand(flag, command);
-    }
-
-    private static void removeFlagFromCommand(String flag, List<String> command) {
-        String flagString = "--" + flag;
-        String abbreviatedFlagString = "-" + flag.charAt(0);
-
-        for (int i = 0; i < command.size(); i++)
-            if (flagString.equals(command.get(i))
-                    || abbreviatedFlagString.equals(command.get(i))) {
-                command.remove(i);
-                return;
-            }
-    }
-
-    private static String[] extractArgumentsFromCommand(String[] argumentNames, List<String> command) {
+    private static String[] extractArgumentsFromCommand(String[] argumentNames, String[] command) {
         String[] arguments = new String[argumentNames.length];
 
         for (int i = 0; i < argumentNames.length; i++)
@@ -109,23 +71,23 @@ public abstract class AbstractView {
         return arguments;
     }
 
-    private static String extractArgumentFromCommand(String argumentName, List<String> command) {
+    private static String extractArgumentFromCommand(String argumentName, String[] command) {
         String commandString = "--" + argumentName;
         String abbreviatedCommandString = "-" + argumentName.charAt(0);
 
-        for (int j = 0; j < command.size() - 1; j++)
-            if (commandString.equals(command.get(j))
-                    || abbreviatedCommandString.equals(command.get(j))) {
-                String argument = command.get(j + 1);
-                command.remove(j);
-                command.remove(j);
-                return argument;
+        for (int j = 0; j < command.length - 1; j++)
+            if (commandString.equals(command[j])
+                    || abbreviatedCommandString.equals(command[j])) {
+                return command[j + 1];
             }
-        return null;
+        throw new GameErrorException(INVALID_COMMAND_MESSAGE, new NullPointerException());
     }
 
     public static boolean[] findFlags(String[] flags, String input) {
-        String[] command = input.split(" ");
+        return findFlags(flags, input.split(" "));
+    }
+
+    public static boolean[] findFlags(String[] flags, String[] command) {
         boolean[] isFlagFound = new boolean[flags.length];
 
         for (int i = 0; i < flags.length; i++)
