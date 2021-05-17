6448a3bbd716391a2f56c770d67291b224af9b70
SalehShamloo <SLHHero@gmail.com>
2021-05-17 12:16:24 +0430

    added missing constructors

--- Yu-Gi-Oh/src/main/java/exception/GameErrorException.java
+++ Yu-Gi-Oh/src/main/java/exception/GameErrorException.java
@@ -1,7 +1,20 @@
 package exception;
 
 public class GameErrorException extends RuntimeException {
+
+    public GameErrorException() {
+        super();
+    }
+
     public GameErrorException(String message) {
         super(message);
+    }
+
+    public GameErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GameErrorException(Throwable cause) {
+        super(cause);
     }
 }
