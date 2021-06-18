2563640aa1791c7c7ce60abf969a7f9c1d9aebb5
mahdiabbastabaar <73561957+mahdiabbastabaar@users.noreply.github.com>
2021-06-18 15:32:03 +0430

    add allCards field

--- Yu-Gi-Oh/src/main/java/model/cardtemplate/Card.java
+++ Yu-Gi-Oh/src/main/java/model/cardtemplate/Card.java
@@ -1,15 +1,23 @@
 package model.cardtemplate;
 
+import java.util.ArrayList;
+
 public abstract class Card {
 
     protected final String name;
     protected final Effect effect;
     protected final String description;
+    protected static final ArrayList<Card> allCards;
+
+    static {
+        allCards = new ArrayList<>();
+    }
 
     public Card(String name, String description, Effect effect) {
         this.name = name;
         this.description = description;
         this.effect = effect;
+        allCards.add(this);
     }
 
     public String getName() {
