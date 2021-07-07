387136e1563f48d7e3322ea6c2e5d17a13f2f951
slhshamloo <slhhero@gmail.com>
2021-07-07 09:38:35 +0430

    check empty fields

--- Yu-Gi-Oh/src/main/java/controller/LoginController.java
+++ Yu-Gi-Oh/src/main/java/controller/LoginController.java
@@ -9,10 +9,19 @@ public class LoginController extends Controller {
     public final Userbase userbase = DATABASE.getUserbase();
 
     public void login(String username, String password) {
+        checkEmptyFields(username, password);
+
         User user = userbase.getUserByUsername(username);
         if (user == null || !user.isPasswordCorrect(password))
             throw new GameErrorException("Username and password didn't match!");
 
         Controller.user = user;
+    }
+
+    private void checkEmptyFields(String username, String password) {
+        if (username.length() == 0)
+            throw new GameErrorException("Enter username");
+        if (password.length() == 0)
+            throw new GameErrorException("Enter password");
     }
 }
