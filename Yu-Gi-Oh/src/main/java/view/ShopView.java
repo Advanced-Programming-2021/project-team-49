dd5d01b866208193dbd8d66baa4d2e7bbedc0770
mahdiabbastabaar <73561957+mahdiabbastabaar@users.noreply.github.com>
2021-07-08 16:26:22 +0430

    shop progress

--- Yu-Gi-Oh/src/main/java/view/ShopView.java
+++ Yu-Gi-Oh/src/main/java/view/ShopView.java
@@ -2,9 +2,12 @@ package view;
 
 import controller.ShopController;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import model.cardtemplate.CardTemplate;
 
@@ -22,15 +25,20 @@ public class ShopView extends View{
     private VBox root;
 
     public void initialize() {
-        int columns = (int) Math.ceil(allCards.size() / 8);
-        for (int i = 0; i < 8; i++) {
+        int columns = (int) Math.ceil(allCards.size() / 12);
+        for (int i = 0; i < 12; i++) {
             for (int j = 0; j < columns; j++) {
                 Rectangle rectangle = new Rectangle();
                 rectangle.setHeight(61.4);
                 rectangle.setWidth(42.1);
+                if (allCards.get(12 * j + i) != null) {
+                    ImagePattern image = new ImagePattern(new Image(allCards.get(12 * j + i).getCardPicPath()));
+                    rectangle.setFill(image);
+                }
                 gridPane.add(rectangle, i, j);
             }
         }
+
     }
 
     public void backToMainMenu(MouseEvent mouseEvent) throws IOException {
