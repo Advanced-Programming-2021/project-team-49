23202713acdd9df77e4745877d558246f787db8b
mahdiabbastabaar <73561957+mahdiabbastabaar@users.noreply.github.com>
2021-07-08 21:23:25 +0430

    یا رب مباد آنکه صدا معتبر شود

--- Yu-Gi-Oh/src/main/java/view/ShopView.java
+++ Yu-Gi-Oh/src/main/java/view/ShopView.java
@@ -1,15 +1,19 @@
 package view;
 
 import controller.ShopController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import model.cardtemplate.CardTemplate;
+import view.popup.ShopPopUp;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,10 +39,35 @@ public class ShopView extends View{
                     ImagePattern image = new ImagePattern(new Image(allCards.get(12 * j + i).getCardPicPath()));
                     rectangle.setFill(image);
                 }
+                rectangle.setCursor(Cursor.HAND);
+                int finalI = i;
+                int finalJ = j;
+                rectangle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        rectangle.setEffect(new DropShadow());
+                    }
+                });
+                rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        selectCardToBuy(finalI, finalJ);
+                    }
+                });
+                rectangle.setOnMouseExited(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        rectangle.setEffect(null);
+                    }
+                });
                 gridPane.add(rectangle, i, j);
             }
         }
 
+    }
+
+    public void selectCardToBuy(int i, int j) {
+        new ShopPopUp(allCards.get(12 * j + i), root).initialize();
     }
 
     public void backToMainMenu(MouseEvent mouseEvent) throws IOException {
---
+++ Yu-Gi-Oh/src/main/java/view/popup/ShopPopUp.java
@@ -0,0 +1,62 @@
+package view.popup;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
+import model.cardtemplate.CardTemplate;
+
+public class ShopPopUp {
+
+    private final CardTemplate card;
+    private final Stage stage;
+    private final Parent root;
+
+    public ShopPopUp(CardTemplate card, Parent root) {
+        this.stage = new Stage();
+        this.card = card;
+        this.root = root;
+    }
+
+    public void initialize() {
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPrefWidth(300);
+        borderPane.setPrefHeight(380);
+
+        Button buyButton = new Button("Buy");
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setOnMouseClicked(mouseEvent -> stage.close());
+
+        borderPane.setBottom(buyButton);
+        borderPane.setBottom(cancelButton);
+
+        ImageView image = new ImageView(new Image(card.getCardPicPath()));
+        image.setFitHeight(255);
+        image.setPreserveRatio(true);
+        borderPane.setCenter(image);
+
+
+
+
+        show(borderPane);
+    }
+
+    public void show(Parent pane) {
+        Scene popUpScene = new Scene(pane);
+        stage.setScene(popUpScene);
+        stage.initOwner(root.getScene().getWindow());
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+        Window outerWindow = root.getScene().getWindow();
+        Window innerWindow = popUpScene.getWindow();
+        innerWindow.setX(outerWindow.getX() + (outerWindow.getWidth() / 2) - (150));
+        innerWindow.setY(outerWindow.getY() + (outerWindow.getHeight() / 2) - (190));
+        stage.show();
+    }
+}
