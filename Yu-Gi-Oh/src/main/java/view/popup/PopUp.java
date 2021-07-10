474f3b10d2aa23d0beea70ddaf0267a1a61f71b8
slhshamloo <slhhero@gmail.com>
2021-07-10 22:36:00 +0430

    fixed popup white corners

--- Yu-Gi-Oh/src/main/java/view/popup/PopUp.java
+++ Yu-Gi-Oh/src/main/java/view/popup/PopUp.java
@@ -2,6 +2,7 @@ package view.popup;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -24,10 +25,12 @@ public abstract class PopUp {
 
     public void show(Parent parent, double parentHeight, double parentWidth) {
         Scene popUpScene = new Scene(parent);
+        popUpScene.setFill(Color.TRANSPARENT);
         stage.setScene(popUpScene);
         stage.initOwner(root.getScene().getWindow());
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
         Window outerWindow = root.getScene().getWindow();
         Window innerWindow = popUpScene.getWindow();
         innerWindow.setX(outerWindow.getX() + (outerWindow.getWidth() / 2) - (parentWidth / 2));
