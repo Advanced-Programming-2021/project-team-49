4db47ed03dbc1884488403ae70aa63bc222728ef
Amin <seyedaminahmadzadeh@gmail.com>
2021-07-10 13:22:03 +0430

    added card in hand test. need to add logic

--- Yu-Gi-Oh/src/main/java/view/DuelView.java
+++ Yu-Gi-Oh/src/main/java/view/DuelView.java
@@ -2,7 +2,11 @@ package view;
 
 import controller.DuelController;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -16,6 +20,7 @@ import java.util.List;
 
 public class DuelView extends View {
 
+    private static final String UNKNOWN_CARD_PATH = View.class.getResource("/cards/Unknown.jpg").toExternalForm();
 
     private DuelController controller;
 
@@ -26,7 +31,7 @@ public class DuelView extends View {
     @FXML
     private Text description;
 
-    // TODO public modifier for accecing from another stage
+    // TODO public modifier for accessing from another stage
     public VBox phaseButtonBox;
     public Pane defenderSidePane;
     public Pane fieldPane;
@@ -37,6 +42,13 @@ public class DuelView extends View {
     public GridPane attackerSpellZone;
     public GridPane attackerHand;
     public Pane attackerSidePane;
+
+    public void initialize() {
+        for (int i = 0; i < 5; i++) {
+            defenderHand.add(createCardInHandImage(null, true), i, 0);
+        }
+    }
+
 
     public static void showCardInfoStringView(Card card) {
     }
@@ -69,5 +81,44 @@ public class DuelView extends View {
 
     public void enterDuelMenu() throws IOException {
         enterNewMenu("/fxml/duelmenu.fxml", root);
+    }
+
+    public ImageView createCardInHandImage(CardTemplate card, boolean opponent) {
+        // TODO opponent boolean could be removed, instead if card is null
+        ImageView cardImage;
+        if (opponent)
+            cardImage = new ImageView(new Image(UNKNOWN_CARD_PATH));
+        else
+            cardImage = new ImageView(new Image(card.getCardPicPath()));
+
+        cardImage.setFitWidth(95);
+        cardImage.setPreserveRatio(true);
+        cardImage.setCursor(Cursor.HAND);
+
+        cardImage.setOnMouseEntered(mouseEvent -> {
+            cardImage.setEffect(new DropShadow());
+
+            if (opponent) {
+                image.setImage(new Image(UNKNOWN_CARD_PATH));
+                description.setText("");
+            } else {
+                image.setImage(cardImage.getImage());
+                description.setText(card.getDescription());
+            }
+
+            mouseEvent.consume();
+        });
+        cardImage.setOnMouseExited(mouseEvent -> cardImage.setEffect(null));
+
+        cardImage.setOnMouseClicked(mouseEvent -> {
+            // TODO main idea of handling is a bit tricky
+            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+                // TODO summon monster, activate spell, set trap
+            } else if (mouseEvent.getButton() == MouseButton.SECONDARY) {
+                // TODO set monster, set spell
+            }
+        });
+
+        return cardImage;
     }
 }
