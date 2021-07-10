92206ca68fae466f378d64dc4a620fecd3fc6c40
mahdiabbastabaar <73561957+mahdiabbastabaar@users.noreply.github.com>
2021-07-10 22:17:00 +0430

    import

--- Yu-Gi-Oh/src/main/java/view/ImportExportMenuView.java
+++ Yu-Gi-Oh/src/main/java/view/ImportExportMenuView.java
@@ -1,12 +1,14 @@
 package view;
 
 import controller.ImportExportController;
-import exception.GameErrorException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ImportExportMenuView extends View {
@@ -14,16 +16,23 @@ public class ImportExportMenuView extends View {
     private final ImportExportController controller = new ImportExportController();
 
     @FXML
+    public HBox filesToChoose;
+    @FXML
     private BorderPane root;
 
+
     public void initialize() {
+
     }
-
-
 
     public void backToMainMenu(MouseEvent mouseEvent) throws IOException {
         enterNewMenu("/fxml/mainmenu.fxml", root);
     }
 
-
+    public void chooseFiles() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        File file = fileChooser.showOpenDialog(stage);
+        controller.importCard(file.getPath());
+    }
 }
