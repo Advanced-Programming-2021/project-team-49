0d760ee244f7715adb3d497bf59fe434109faf32
mahdiabbastabaar <73561957+mahdiabbastabaar@users.noreply.github.com>
2021-07-11 20:45:37 +0430

    create cards (bugfull)

--- Yu-Gi-Oh/src/main/java/controller/CreateCardController.java
+++ Yu-Gi-Oh/src/main/java/controller/CreateCardController.java
@@ -1,5 +1,65 @@
 package controller;
 
-public class CreateCardController extends Controller{
+import exception.GameErrorException;
+import model.cardtemplate.*;
+
+public class CreateCardController extends Controller {
+
+    private int price = 0;
+    private String cardType;
+    private String cardName;
+
+    public CreateCardController() {
+    }
+
+    public void cardType(String type) {
+        switch (type) {
+            case "Monster":
+                price += 700;
+                cardType = "Monster";
+                break;
+            case "Spell":
+                price += 400;
+                cardType = "Spell";
+                break;
+            case "Trap":
+                price += 400;
+                cardType = "Trap";
+                break;
+            default:
+                throw new GameErrorException("card type is wrong");
+        }
+    }
+
+    public void cardName(String name) {
+        this.cardName = name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void createCard() {
+        switch (cardType) {
+            case "Monster":
+                MonsterCard monsterCard = new MonsterCard(this.cardName, "", Effect.NONE, 1, null,
+                        CardType.NORMAL, MonsterType.BEAST_WARRIOR, 300, 300,
+                        getClass().getResource("image/rawCard.png").toExternalForm(), this.price);
+                break;
+            case "Spell": {
+                SpellTrapCard spellTrapCard = new SpellTrapCard(this.cardName, "", Effect.NONE, EffectType.NORMAL,
+                        Status.LIMITED, SpellTrapType.SPELL,
+                        getClass().getResource("image/rawCard.png").toExternalForm(), this.price);
+                break;
+            }
+            case "Trap": {
+                SpellTrapCard spellTrapCard = new SpellTrapCard(this.cardName, "", Effect.NONE, EffectType.NORMAL,
+                        Status.LIMITED, SpellTrapType.TRAP,
+                        getClass().getResource("image/rawCard.png").toExternalForm(), this.price);
+                break;
+            }
+        }
+    }
+
 
 }
--- Yu-Gi-Oh/src/main/java/view/CreateCardView.java
+++ Yu-Gi-Oh/src/main/java/view/CreateCardView.java
@@ -1,24 +1,72 @@
 package view;
 
+import controller.CreateCardController;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class CreateCardView extends View{
 
+    private final CreateCardController controller = new CreateCardController();
+
+    @FXML
+    private Text price;
+    @FXML
+    private TextField nameField;
     @FXML
     private VBox root;
     @FXML
-    private CheckBox types;
+    private RadioButton monster;
+    @FXML
+    private RadioButton spell;
+    @FXML
+    private RadioButton trap;
+    @FXML
+    private final ToggleGroup toggleGroup = new ToggleGroup();
 
     public void initialize() {
-
+        monster.setToggleGroup(toggleGroup);
+        spell.setToggleGroup(toggleGroup);
+        trap.setToggleGroup(toggleGroup);
+        price.setText("Final price: " + controller.getPrice());
     }
+
 
     public void backToMainMenu(MouseEvent mouseEvent) throws IOException {
         enterNewMenu("/fxml/mainmenu.fxml", root);
+    }
+
+    public void createCard(MouseEvent mouseEvent) {
+        controller.createCard();
+    }
+
+    public void setMonsterType(MouseEvent mouseEvent) {
+        controller.cardType("Monster");
+        price.setText("Final price: " + controller.getPrice());
+    }
+
+    public void setSpellType(MouseEvent mouseEvent) {
+        controller.cardType("Monster");
+        price.setText("Final price: " + controller.getPrice());
+    }
+
+    public void setTrapType(MouseEvent mouseEvent) {
+        controller.cardType("Monster");
+        price.setText("Final price: " + controller.getPrice());
+    }
+
+
+    public void setName(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            controller.cardName(nameField.getText());
+        }
     }
 }
